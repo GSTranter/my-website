ca875b6658a53437ce06f6fe2e82531a80e46426
George Tranter <georgetranter@hotmail.com>
2025-05-24 17:37:07 +0100

    WIP: Sanity types not working

--- .env.d.ts
+++ .env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="astro/client" />
 /// <reference types="@sanity/astro/module" />
+
+
+declare module 'sanity:client' {
+  export const sanityClient: import('@sanity/client').SanityClient;
+}
+
+declare module "sanity:studio" {
+  export const studioConfig: import("sanity").Config;
+}
